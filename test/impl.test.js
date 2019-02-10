56ca6fbc6ead2b27dc50bb09162671df71d0dc8c
yumetodo <yume-wikijp@live.jp>
2019-02-10 15:26:09 +0900

    fix(test): write markdown text outside

--- test/impl.test.js
+++ test/impl.test.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import impl from '../src/impl'
+const fs = require('fs').promises;
 const text1 = `arikitari
 na
 sekai`;
@@ -8,20 +9,19 @@ test('impl.listUpLineEnd', t => {
     t.true(3 === re.length);
     t.deepEqual(re, [9,12,18])
 });
-/* eslint-disable max-len */
-const text2 = `# [![AVA](media/header.png)](https://ava.li)
-
-[![Build Status](https://travis-ci.org/avajs/ava.svg?branch=master)](https://travis-ci.org/avajs/ava)  [![Coverage Status](https://codecov.io/gh/avajs/ava/branch/master/graph/badge.svg)](https://codecov.io/gh/avajs/ava/branch/master) [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/xojs/xo) [![Join the community on Spectrum](https://withspectrum.github.io/badge/badge.svg)](https://spectrum.chat/ava)
-[![Mentioned in Awesome Node.js](https://awesome.re/mentioned-badge.svg)](https://github.com/sindresorhus/awesome-nodejs)
-
-Testing can be a drag. AVA helps you get it done. AVA is a test runner for Node.js with a concise API, detailed error output, embrace of new language features and process isolation that let you write tests more effectively. So you can ship more awesome code. 🚀
-
-Follow the [AVA Twitter account](https://twitter.com/ava__js) for updates.`
-/* eslint-enable max-len */
-test('impl.listUpParagraphDelim', t => {
+test('impl.listUpParagraphDelim', async t => {
+    const text2 = await fs.readFile('./test/input/text2.md', 'utf8');
+    //force LF
     const text = text2.replace(/\r\n?/g,'\n');
     const lineEndList = impl.listUpLineEnd(text);
     const re = impl.listUpParagraphDelim(lineEndList);
-    t.true(4 === re.length);
-    t.deepEqual(re, [[44,45],[614,615], [877, 878], [953, 953]]);
+    t.deepEqual(re, [
+        [8, 9],
+        [41, 42],
+        [70, 71],
+        [149, 150],
+        [271, 272],
+        [301, 302],
+        [336, 337]
+    ]);
 });
---
+++ test/input/text2.md
@@ -0,0 +1,27 @@
+## Usage
+
+To install and set up AVA, run:
+
+```console
+npm init ava
+```
+
+Your `package.json` will then look like this (exact version notwithstanding):
+
+```json
+{
+	"name": "awesome-package",
+	"scripts": {
+		"test": "ava"
+	},
+	"devDependencies": {
+		"ava": "^1.0.0"
+	}
+}
+```
+
+Or if you prefer using Yarn:
+
+```console
+yarn add ava --dev
+```
