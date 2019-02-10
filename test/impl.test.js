a4c07397776d5bff6152677c3404d4e777c1edc1
yumetodo <yume-wikijp@live.jp>
2019-02-10 13:27:35 +0900

    chore(test): rename impl.listUpLineEnd test name

--- test/impl.test.js
+++ test/impl.test.js
@@ -3,7 +3,7 @@ import impl from '../src/impl'
 const text1 = `arikitari
 na
 sekai`;
-test('listUpLineEnd', t => {
+test('impl.listUpLineEnd', t => {
     const re = impl.listUpLineEnd(text1);
     t.true(3 === re.length);
     t.deepEqual(re, [9,12,18])
