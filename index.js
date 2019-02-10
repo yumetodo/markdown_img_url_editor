1e22e0335b78753b6507ff8c403220a0b388c69d
yumetodo <yume-wikijp@live.jp>
2019-02-10 13:02:47 +0900

    fix: divide impl to other file for unit test

--- index.js
+++ index.js
@@ -1,122 +1,5 @@
 const bs = require('@extra-array/binary-search.closest');
-/**
- * list up LF position
- * @param {string} s string
- */
-const listUpLineEnd = (s) => {
-    let re = [];
-    for(let i = 0; -1 !== (i = s.indexOf('\n')); ++i) {
-        re.push(i);
-    }
-    return re;
-};
-/**
- * list up paragraph
- * @param {number[]} lineEndList created by `listUpLineEnd`
- * @returns {number[][]} array of paragraph delim range
- */
-const listUpParagraphDelim = (lineEndList) => {
-    let re = [];
-    /** @types {number|null} */
-    let pre = null;
-    for(const n of lineEndList) {
-        if(null === pre) {
-            pre = n;
-            continue;
-        }
-        if(pre + 1 === n) {
-            if (re[re.length - 1][1] === pre) {
-                re[re.length - 1][1] = n;
-            } else {
-                re.push([pre, n]);
-            }
-        }
-        pre = n;
-    }
-    const last = lineEndList[lineEndList.length -1];
-    if(re[re.length - 1][1] !== last) {
-        re.push([last, last]);
-    }
-    return re;
-};
-/**
- * list up code block range
- * @param {string} markdownText markdown text
- * @param {number[]} lineEndList lineEndList created by `listUpLineEnd`
- * @returns {number[][]} array of code block range
- */
-const listUpCodeBlockRange = (markdownText, lineEndList) => {
-    /** @types {number[][]} */
-    let re = [];
-    /** @types {number | null} */
-    let beginPos = null;
-    if(markdownText.startsWith('```')) {
-        beginPos = 0;
-    }
-    for(const preLineEnd of lineEndList) {
-        if(markdownText.startsWith('```', preLineEnd + 1)) {
-            if(null === beginPos) {
-                beginPos = preLineEnd + 1;
-            } else if('\r' === markdownText.charAt(preLineEnd + 4) || '\n' === markdownText.charAt(preLineEnd + 4)) {
-                re.push([beginPos, preLineEnd + 4]);
-            }
-        }
-    }
-    return re;
-};
-/**
- * find nearest paragraph end pos
- * @param {number[][]} paragraphList paragraphList created by `listUpParagraphDelim`
- * @param {number} pos
- * @param {number} begin search begin index in `paragraphList`
- * @returns {[number, number]} the pair of find nearest paragraph end pos and the index in `paragraphList`
- */
-const findNearestParagraphEndPos = (paragraphList, pos, begin) => {
-    /** @types {number} */
-    const index = bs(paragraphList, pos, (a, b) => a[0] - b, null, begin);
-    return [paragraphList[index][0], index];
-}
-/**
- * list up code tag range
- * @param {string} markdownText markdown text
- * @param {number[][]} paragraphList created by `listUpParagraphDelim`
- * @returns {number[][]} array of code tag range
- */
-const listUpCodeRange = (markdownText, paragraphList) => {
-    /** @types {number[][]} */
-    let re = [];
-    /** @types {number|null} */
-    let pre = null;
-    let hintPos = 0;
-    for(let pos = 0; -1 !== (pos = markdownText.indexOf('`', pos)); ++pos) {
-        if(pre === null) {
-            pre = pos;
-            continue;
-        }
-        const [nearestParagraphEndPos, index] = findNearestParagraphEndPos(paragraphList, pre, hintPos);
-        if(nearestParagraphEndPos < pos) {
-            pre = pos;
-            hintPos = index;
-        } else {
-            re.push([pre, pos]);
-            pre = null;
-            hintPos = Math.max(0, index - 1);
-        }
-    }
-    return re;
-};
-/**
- * check weather pos is in range
- * @param {number[][]} rangeList array of range
- * @param {number} pos
- * @param {number} begin
- * @returns {[boolean, number]}
- */
-const isInRange = (rangeList, pos, begin) => {
-    /** @types {number} */
-    const index = bs(rangeList, pos, (a, b) => a[1] - b, null, begin);
-    return [(rangeList[index][0] <= pos && pos <= rangeList[index][1]), index];
-}
+const impl = require('./src/impl');
 /**
  * edit markdown img src
  * @param {string} markdownText markdown text
@@ -129,10 +12,10 @@ const markdownImgUrlEditor = async (markdownText, converter, beforeCollectCallba
     let strings = [];
     /** @types {Promise<string>[]} */
     let promiseStrings = [];
-    const lineEndList = listUpLineEnd(markdownText);
-    const paragraphList = listUpParagraphDelim(lineEndList);
-    const codeRangeList = listUpCodeRange(markdownText, paragraphList);
-    const codeBlockRangeList = listUpCodeBlockRange(markdownText, lineEndList);
+    const lineEndList = impl.listUpLineEnd(markdownText);
+    const paragraphList = impl.listUpParagraphDelim(lineEndList);
+    const codeRangeList = impl.listUpCodeRange(markdownText, paragraphList);
+    const codeBlockRangeList = impl.listUpCodeBlockRange(markdownText, lineEndList);
     let imageBlockBeginPos = 0, codeBlockRangeHintPos = 0, codeRangeHintPos = 0, pre = 0;
     /**
      * skip code tag
@@ -140,13 +23,13 @@ const markdownImgUrlEditor = async (markdownText, converter, beforeCollectCallba
      * @returns {boolean}
      */
     const skipCode = (pos) => {
-        const [inCodeBlockRange, index1] = isInRange(codeBlockRangeList, pos, codeBlockRangeHintPos);
+        const [inCodeBlockRange, index1] = impl.isInRange(codeBlockRangeList, pos, codeBlockRangeHintPos);
         if(inCodeBlockRange) {
             imageBlockBeginPos = codeBlockRangeList[index1][1] + 1;
             codeBlockRangeHintPos = index1 + 1;
             return true;
         }
-        const [inCodeRange, index2] = isInRange(codeRangeList, pos, codeRangeHintPos);
+        const [inCodeRange, index2] = impl.isInRange(codeRangeList, pos, codeRangeHintPos);
         if(inCodeRange) {
             imageBlockBeginPos = codeRangeList[index2][1] + 1;
             codeRangeHintPos = index2 + 1;
---
+++ src/impl.js
@@ -0,0 +1,125 @@
+const bs = require('@extra-array/binary-search.closest');
+class Impl {
+    /**
+     * list up LF position
+     * @param {string} s string
+     */
+    static listUpLineEnd(s) {
+        let re = [];
+        for(let i = 0; -1 !== (i = s.indexOf('\n')); ++i) {
+            re.push(i);
+        }
+        return re;
+    }
+    /**
+     * list up paragraph
+     * @param {number[]} lineEndList created by `listUpLineEnd`
+     * @returns {number[][]} array of paragraph delim range
+     */
+    static listUpParagraphDelim(lineEndList) {
+        let re = [];
+        /** @types {number|null} */
+        let pre = null;
+        for(const n of lineEndList) {
+            if(null === pre) {
+                pre = n;
+                continue;
+            }
+            if(pre + 1 === n) {
+                if (re[re.length - 1][1] === pre) {
+                    re[re.length - 1][1] = n;
+                } else {
+                    re.push([pre, n]);
+                }
+            }
+            pre = n;
+        }
+        const last = lineEndList[lineEndList.length -1];
+        if(re[re.length - 1][1] !== last) {
+            re.push([last, last]);
+        }
+        return re;
+    }
+    /**
+     * list up code block range
+     * @param {string} markdownText markdown text
+     * @param {number[]} lineEndList lineEndList created by `listUpLineEnd`
+     * @returns {number[][]} array of code block range
+     */
+    static listUpCodeBlockRange(markdownText, lineEndList) {
+        /** @types {number[][]} */
+        let re = [];
+        /** @types {number | null} */
+        let beginPos = null;
+        if(markdownText.startsWith('```')) {
+            beginPos = 0;
+        }
+        for(const preLineEnd of lineEndList) {
+            if(markdownText.startsWith('```', preLineEnd + 1)) {
+                if(null === beginPos) {
+                    beginPos = preLineEnd + 1;
+                } else if(
+                    '\r' === markdownText.charAt(preLineEnd + 4) ||
+                    '\n' === markdownText.charAt(preLineEnd + 4)
+                ) {
+                    re.push([beginPos, preLineEnd + 4]);
+                }
+            }
+        }
+        return re;
+    }
+    /**
+     * find nearest paragraph end pos
+     * @param {number[][]} paragraphList paragraphList created by `listUpParagraphDelim`
+     * @param {number} pos
+     * @param {number} begin search begin index in `paragraphList`
+     * @returns {[number, number]} the pair of find nearest paragraph end pos and the index in `paragraphList`
+     */
+    static findNearestParagraphEndPos(paragraphList, pos, begin) {
+        /** @types {number} */
+        const index = bs(paragraphList, pos, (a, b) => a[0] - b, null, begin);
+        return [paragraphList[index][0], index];
+    }
+    /**
+     * list up code tag range
+     * @param {string} markdownText markdown text
+     * @param {number[][]} paragraphList created by `listUpParagraphDelim`
+     * @returns {number[][]} array of code tag range
+     */
+    static listUpCodeRange(markdownText, paragraphList) {
+        /** @types {number[][]} */
+        let re = [];
+        /** @types {number|null} */
+        let pre = null;
+        let hintPos = 0;
+        for(let pos = 0; -1 !== (pos = markdownText.indexOf('`', pos)); ++pos) {
+            if(pre === null) {
+                pre = pos;
+                continue;
+            }
+            const [nearestParagraphEndPos, index] = this.findNearestParagraphEndPos(paragraphList, pre, hintPos);
+            if(nearestParagraphEndPos < pos) {
+                pre = pos;
+                hintPos = index;
+            } else {
+                re.push([pre, pos]);
+                pre = null;
+                hintPos = Math.max(0, index - 1);
+            }
+        }
+        return re;
+    }
+    /**
+     * check weather pos is in range
+     * @param {number[][]} rangeList array of range
+     * @param {number} pos
+     * @param {number} begin
+     * @returns {[boolean, number]}
+     */
+    static isInRange(rangeList, pos, begin) {
+        /** @types {number} */
+        const index = bs(rangeList, pos, (a, b) => a[1] - b, null, begin);
+        return [(rangeList[index][0] <= pos && pos <= rangeList[index][1]), index];
+    }
+}
+module.exports = Impl;
