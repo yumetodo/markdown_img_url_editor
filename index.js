1b7fdfd4333ff96771b38b177eabf21699d302f6
yumetodo <yume-wikijp@live.jp>
2019-02-10 20:01:21 +0900

    feat: introduce prettier

--- index.js
+++ index.js
@@ -8,76 +8,77 @@ const impl = require('./src/impl');
  * @returns {string}
  */
 const markdownImgUrlEditor = async (markdownText, converter, beforeCollectCallback) => {
-    /** @types {(number | string)[]} */
-    let strings = [];
-    /** @types {Promise<string>[]} */
-    let promiseStrings = [];
-    const lineEndList = impl.listUpLineEnd(markdownText);
-    const paragraphList = impl.listUpParagraphDelim(lineEndList);
-    const codeRangeList = impl.listUpCodeRange(markdownText, paragraphList);
-    const codeBlockRangeList = impl.listUpCodeBlockRange(markdownText, lineEndList);
-    let imageBlockBeginPos = 0, codeBlockRangeHintPos = 0, codeRangeHintPos = 0, pre = 0;
-    /**
-     * skip code tag
-     * @param {number} pos
-     * @returns {boolean}
-     */
-    const skipCode = (pos) => {
-        const [inCodeBlockRange, index1] = impl.isInRange(codeBlockRangeList, pos, codeBlockRangeHintPos);
-        if(inCodeBlockRange) {
-            imageBlockBeginPos = codeBlockRangeList[index1][1] + 1;
-            codeBlockRangeHintPos = index1 + 1;
-            return true;
-        }
-        const [inCodeRange, index2] = impl.isInRange(codeRangeList, pos, codeRangeHintPos);
-        if(inCodeRange) {
-            imageBlockBeginPos = codeRangeList[index2][1] + 1;
-            codeRangeHintPos = index2 + 1;
-        }
-        return inCodeRange;
-    };
-    while(-1 != (imageBlockBeginPos = markdownText.indexOf('![', imageBlockBeginPos))) {
-        if(skipCode(imageBlockBeginPos)) continue;
-        const lineEndPos = bs(lineEndList, imageBlockBeginPos);
-        /**
-         * find, skip, happy!
-         * @param {number} pos
-         * @param {string} searchStr
-         */
-        const find = (pos, searchStr) => {
-            while(
-                -1 !== (pos = markdownText.indexOf(searchStr, pos)) &&
-                pos < lineEndPos &&
-                '\\' === markdownText.charAt(pos)
-            );
-            if(-1 === pos) {
-                return pos;
-            }
-            if(skipCode(pos)) return null;
-            if(lineEndPos <= pos) {
-                imageBlockBeginPos = lineEndPos + 1;
-                return null;
-            }
-            return pos;
-        };
-        const imageBlockAltLastPosResult = find(imageBlockBeginPos + 2, '](');
-        if(null === imageBlockAltLastPosResult) continue;
-        if(-1 === imageBlockAltLastPosResult) break;
-        const imageBlockLastPosResult = find(imageBlockAltLastPosResult + 2, ')');
-        if(null === imageBlockLastPosResult) continue;
-        if(-1 === imageBlockLastPosResult) break;
-        //append before image URL
-        strings.push(markdownText.substring(pre, imageBlockAltLastPosResult + 2));
-        pre = imageBlockLastPosResult;
-        promiseStrings.push(
-            converter(markdownText.substring(imageBlockAltLastPosResult + 2, imageBlockLastPosResult))
-        );
-        strings.push(promiseStrings.length - 1);
+  /** @types {(number | string)[]} */
+  let strings = [];
+  /** @types {Promise<string>[]} */
+  let promiseStrings = [];
+  const lineEndList = impl.listUpLineEnd(markdownText);
+  const paragraphList = impl.listUpParagraphDelim(lineEndList);
+  const codeRangeList = impl.listUpCodeRange(markdownText, paragraphList);
+  const codeBlockRangeList = impl.listUpCodeBlockRange(markdownText, lineEndList);
+  let imageBlockBeginPos = 0,
+    codeBlockRangeHintPos = 0,
+    codeRangeHintPos = 0,
+    pre = 0;
+  /**
+   * skip code tag
+   * @param {number} pos
+   * @returns {boolean}
+   */
+  const skipCode = pos => {
+    const [inCodeBlockRange, index1] = impl.isInRange(codeBlockRangeList, pos, codeBlockRangeHintPos);
+    if (inCodeBlockRange) {
+      imageBlockBeginPos = codeBlockRangeList[index1][1] + 1;
+      codeBlockRangeHintPos = index1 + 1;
+      return true;
     }
-    //append rest
-    strings.push(markdownText.substring(pre));
-    if(null != beforeCollectCallback) await beforeCollectCallback();
-    const promiseResultStrings = await Promise.all(promiseStrings);
-    return strings.map(e => typeof(e) === 'number' ? promiseResultStrings[e] : e).join('');
+    const [inCodeRange, index2] = impl.isInRange(codeRangeList, pos, codeRangeHintPos);
+    if (inCodeRange) {
+      imageBlockBeginPos = codeRangeList[index2][1] + 1;
+      codeRangeHintPos = index2 + 1;
+    }
+    return inCodeRange;
+  };
+  while (-1 != (imageBlockBeginPos = markdownText.indexOf('![', imageBlockBeginPos))) {
+    if (skipCode(imageBlockBeginPos)) continue;
+    const lineEndPos = bs(lineEndList, imageBlockBeginPos);
+    /**
+     * find, skip, happy!
+     * @param {number} pos
+     * @param {string} searchStr
+     */
+    const find = (pos, searchStr) => {
+      while (
+        -1 !== (pos = markdownText.indexOf(searchStr, pos)) &&
+        pos < lineEndPos &&
+        '\\' === markdownText.charAt(pos)
+      );
+      if (-1 === pos) {
+        return pos;
+      }
+      if (skipCode(pos)) return null;
+      if (lineEndPos <= pos) {
+        imageBlockBeginPos = lineEndPos + 1;
+        return null;
+      }
+      return pos;
+    };
+    const imageBlockAltLastPosResult = find(imageBlockBeginPos + 2, '](');
+    if (null === imageBlockAltLastPosResult) continue;
+    if (-1 === imageBlockAltLastPosResult) break;
+    const imageBlockLastPosResult = find(imageBlockAltLastPosResult + 2, ')');
+    if (null === imageBlockLastPosResult) continue;
+    if (-1 === imageBlockLastPosResult) break;
+    //append before image URL
+    strings.push(markdownText.substring(pre, imageBlockAltLastPosResult + 2));
+    pre = imageBlockLastPosResult;
+    promiseStrings.push(converter(markdownText.substring(imageBlockAltLastPosResult + 2, imageBlockLastPosResult)));
+    strings.push(promiseStrings.length - 1);
+  }
+  //append rest
+  strings.push(markdownText.substring(pre));
+  if (null != beforeCollectCallback) await beforeCollectCallback();
+  const promiseResultStrings = await Promise.all(promiseStrings);
+  return strings.map(e => (typeof e === 'number' ? promiseResultStrings[e] : e)).join('');
 };
 module.exports = markdownImgUrlEditor;
--- test/impl.test.js
+++ test/impl.test.js
@@ -1,41 +1,27 @@
 import test from 'ava';
-import impl from '../src/impl'
+import impl from '../src/impl';
 const fs = require('fs').promises;
 const text1 = `arikitari
 na
 sekai`;
 test('impl.listUpLineEnd', t => {
-    const re = impl.listUpLineEnd(text1);
-    t.true(3 === re.length);
-    t.deepEqual(re, [9,12,18])
+  const re = impl.listUpLineEnd(text1);
+  t.true(3 === re.length);
+  t.deepEqual(re, [9, 12, 18]);
 });
 test('impl.listUpParagraphDelim', async t => {
-    const text2 = await fs.readFile('./test/input/text2.md', 'utf8');
-    //force LF
-    const text = text2.replace(/\r\n?/g,'\n');
-    const lineEndList = impl.listUpLineEnd(text);
-    const re = impl.listUpParagraphDelim(lineEndList);
-    t.deepEqual(re, [
-        [8, 9],
-        [41, 42],
-        [70, 71],
-        [149, 150],
-        [271, 272],
-        [301, 302],
-        [336, 337],
-        [345, 346],
-        [788, 789]
-    ]);
+  const text2 = await fs.readFile('./test/input/text2.md', 'utf8');
+  //force LF
+  const text = text2.replace(/\r\n?/g, '\n');
+  const lineEndList = impl.listUpLineEnd(text);
+  const re = impl.listUpParagraphDelim(lineEndList);
+  t.deepEqual(re, [[8, 9], [41, 42], [70, 71], [149, 150], [271, 272], [301, 302], [336, 337], [345, 346], [788, 789]]);
 });
 test('impl.listUpCodeBlockRange', async t => {
-    const text2 = await fs.readFile('./test/input/text2.md', 'utf8');
-    //force LF
-    const text = text2.replace(/\r\n?/g,'\n');
-    const lineEndList = impl.listUpLineEnd(text);
-    const re = impl.listUpCodeBlockRange(text, lineEndList);
-    t.deepEqual(re, [
-        [43, 70],
-        [151, 271],
-        [303, 336]
-    ]);
+  const text2 = await fs.readFile('./test/input/text2.md', 'utf8');
+  //force LF
+  const text = text2.replace(/\r\n?/g, '\n');
+  const lineEndList = impl.listUpLineEnd(text);
+  const re = impl.listUpCodeBlockRange(text, lineEndList);
+  t.deepEqual(re, [[43, 70], [151, 271], [303, 336]]);
 });
