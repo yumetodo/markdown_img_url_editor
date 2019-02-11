8ac5cf14be54d2f17c9c02bb0b920686e9bd8fdc
yumetodo <yume-wikijp@live.jp>
2019-02-11 11:03:14 +0900

    fix(test): cache input file

--- test/impl.test.js
+++ test/impl.test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import impl from '../src/impl';
-const fs = require('fs').promises;
+const TextCache = require('verifiable-file-read-all-cache');
 const text1 = `arikitari
 na
 sekai`;
@@ -9,18 +9,20 @@ test('impl.listUpLineEnd', t => {
   t.true(3 === re.length);
   t.deepEqual(re, [9, 12, 18]);
 });
+const text2 = new TextCache(
+  './test/input/text2.md',
+  'd270ba28b95e9f256ca2eb993fc0692b9d755c4b397f75ea2d5deb5cf39c49f1'
+);
 test('impl.listUpParagraphDelim', async t => {
-  const text2 = await fs.readFile('./test/input/text2.md', 'utf8');
-  //force LF
-  const text = text2.replace(/\r\n?/g, '\n');
+  await text2.verify((actual, expected) => t.true(actual === expected));
+  const text = await text2.get();
   const lineEndList = impl.listUpLineEnd(text);
   const re = impl.listUpParagraphDelim(lineEndList);
   t.deepEqual(re, [[8, 9], [41, 42], [70, 71], [149, 150], [271, 272], [301, 302], [336, 337], [345, 346], [788, 789]]);
 });
 test('impl.listUpCodeBlockRange', async t => {
-  const text2 = await fs.readFile('./test/input/text2.md', 'utf8');
-  //force LF
-  const text = text2.replace(/\r\n?/g, '\n');
+  await text2.verify((actual, expected) => t.true(actual === expected));
+  const text = await text2.get();
   const lineEndList = impl.listUpLineEnd(text);
   const re = impl.listUpCodeBlockRange(text, lineEndList);
   t.deepEqual(re, [[43, 70], [151, 271], [303, 336]]);
