1c150a1e75bd85a2e4efc5c9e87323a11ad8b061
yumetodo <yume-wikijp@live.jp>
2019-02-14 19:02:47 +0900

    chore: remove compile result from vcs

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * edit markdown img src
- * @param  markdownText markdown text
- * @param converter acccept markdown img src and convert
- * @param beforeCollectCallback called before collect convert result
- */
-export declare function markdownImgUrlEditor(markdownText: string, converter: (src: string) => Promise<string>, beforeCollectCallback: () => void): Promise<string>;
